6bc560350620c43e4ea0952b95b8d194b4f2256e
jinshihua <460795475@qq.com>
2024-06-22 15:18:55 +0800

    B分支写的JshController!!!

---
+++ Insurance/src/dao/JshController.java
@@ -0,0 +1,16 @@
+public class JshController {
+    private String name;
+    private int age;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("这是B分支开发的JshController类！！！");
+    }
+}
